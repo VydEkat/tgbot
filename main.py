0ff19ac42ee72bbe9b7dbc185bb3f6e617dff615
Екатерина <ekaterina@MBP-Ekaterina-3.lan>
2024-03-27 18:39:55 +0300

    v1

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from keyboards import start_keyboard
 
 
 bot = telebot.TeleBot(TOKEN)
-import callbacks, handlers
+from callbacks import *
+from handlers import *
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
